032dc2e3654544e1c5561f97ee7d2131d934b413
dickyrachman <dicky.rachman@dto.go.id>
2022-11-16 09:08:19 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "app.terraform.io/DTO-Dicky/network/google"
   version = "3.4.0"
   # insert required variables here
-}
+
 network_name    = "gaurav-network"
 project_id      = var.project
 subnets = [
@@ -12,3 +12,4 @@ subnets = [
     subnet_region = var.region
   }
 ]
+}
