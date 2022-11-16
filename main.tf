f41902c3bbe7dd765b52441f4a3e059c7f415358
dickyrachman <118149257+dickyrachman@users.noreply.github.com>
2022-11-16 15:41:13 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,8 +48,6 @@ resource "google_compute_instance" "hashicat" {
   zone         = "${var.region}-b"
   machine_type = var.machine_type
   
-
-
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-1804-lts"
